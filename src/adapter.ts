72260cf983d862bf245bd9c4d0a5bfb3811eeb1b
Richard Huang <flyerhzm@gmail.com>
2022-07-23 15:08:40 +0800

    add doc to adapter

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,7 +1,30 @@
 interface Adapter<T> {
+  /**
+   * Get node type of the ast node.
+   * @param node {T} ast node
+   * @returns {string} node type
+   */
   getNodeType(node: T): string;
+
+  /**
+   * Get source code of the ast node.
+   * @param node {T} ast node
+   * @returns {string} source code
+   */
   getSource(node: T): string;
+
+  /**
+   * Get node children of the ast node.
+   * @param node {T} ast node
+   * @returns {T[]} node children
+   */
   getChildren(node: T): T[];
+
+  /**
+   * Get node siblings of the ast node.
+   * @param node {T} ast node
+   * @returns {T[]} node siblings
+   */
   getSiblings(node: T): T[];
 }
 
