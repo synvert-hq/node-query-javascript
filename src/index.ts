043320dd5aff3c3a13046b093814ae8dacc3bde6
Richard Huang <flyerhzm@gmail.com>
2022-08-08 20:28:07 +0800

    do not export handleRecursiveChild and getTargetNode helper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import NodeQuery from "./node-query";
 import Adapter from "./adapter";
 import SyntaxError from "./syntax-error";
-import { handleRecursiveChild, getTargetNode } from "./compiler/helpers";
 
 export default NodeQuery;
-export { Adapter, SyntaxError, handleRecursiveChild, getTargetNode };
+export { Adapter, SyntaxError };
