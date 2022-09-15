16328104c8535facbc947669aa90fdeaadd1e2f3
Richard Huang <flyerhzm@gmail.com>
2022-09-15 13:54:41 +0800

    export QueryOptions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import NodeQuery from "./node-query";
 import Adapter from "./adapter";
 import TypescriptAdapter from "./typescript-adapter";
 import SyntaxError from "./syntax-error";
+import type { QueryOptions } from "./compiler/types";
 
 export default NodeQuery;
-export { Adapter, TypescriptAdapter, SyntaxError };
+export { Adapter, TypescriptAdapter, SyntaxError, QueryOptions };
