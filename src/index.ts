8a94888d499279f610ab04cd9fa82158f0aff049
Richard Huang <flyerhzm@gmail.com>
2023-06-04 17:39:21 +0800

    export GonzalesPeAdapter

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import NodeQuery from "./node-query";
 import Adapter from "./adapter";
 import EspreeAdapter from "./adapter/espree";
 import TypescriptAdapter from "./adapter/typescript";
+import GonzalesPeAdapter from "./adapter/gonzales-pe";
 import SyntaxError from "./syntax-error";
 import type { QueryOptions } from "./compiler/types";
 
 export default NodeQuery;
-export { Adapter, EspreeAdapter, TypescriptAdapter, SyntaxError, QueryOptions };
+export { Adapter, EspreeAdapter, TypescriptAdapter, GonzalesPeAdapter, SyntaxError, QueryOptions };
