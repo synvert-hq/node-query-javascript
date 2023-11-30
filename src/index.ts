7c086dbbcd2204529f887726813ebd5f2200fc0b
Richard Huang <flyerhzm@gmail.com>
2023-11-30 13:36:10 +0800

    no need to expose TypescriptAdapter, EspreeAdapter and GonzalesAdapter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import NodeQuery from "./node-query";
 import Adapter from "./adapter";
-import EspreeAdapter from "./adapter/espree";
-import TypescriptAdapter from "./adapter/typescript";
-import GonzalesPeAdapter from "./adapter/gonzales-pe";
 import SyntaxError from "./syntax-error";
 import type { QueryOptions } from "./compiler/types";
 
 export default NodeQuery;
 export {
   Adapter,
-  EspreeAdapter,
-  TypescriptAdapter,
-  GonzalesPeAdapter,
   SyntaxError,
   QueryOptions,
 };
