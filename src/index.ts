8e3f1cd5a48f731aacc4841e07907d8c5798455f
Richard Huang <flyerhzm@gmail.com>
2023-12-02 13:03:53 +0800

    Revert "no need to expose TypescriptAdapter, EspreeAdapter and GonzalesAdapter"
    This reverts commit 7c086dbbcd2204529f887726813ebd5f2200fc0b.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import NodeQuery from "./node-query";
 import Adapter from "./adapter";
+import EspreeAdapter from "./adapter/espree";
+import TypescriptAdapter from "./adapter/typescript";
+import GonzalesPeAdapter from "./adapter/gonzales-pe";
 import SyntaxError from "./syntax-error";
 import type { QueryOptions } from "./compiler/types";
 
 export default NodeQuery;
-export { Adapter, SyntaxError, QueryOptions };
+export {
+  Adapter,
+  EspreeAdapter,
+  TypescriptAdapter,
+  GonzalesPeAdapter,
+  SyntaxError,
+  QueryOptions,
+};
